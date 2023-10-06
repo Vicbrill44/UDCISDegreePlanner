f2187663414ce7741e7998bb30d761df032f8fda
Victor Vasquez <victorbrillos1@gmail.com>
2023-10-06 03:40:12 -0400

    finished checkanswer and giveattempts components

--- src/form-components/CheckAnswer.tsx
+++ src/form-components/CheckAnswer.tsx
@@ -1,13 +1,29 @@
 import React, { useState } from "react";
+import { Col, Form, Row } from "react-bootstrap";
 
 export function CheckAnswer({
     expectedAnswer
 }: {
     expectedAnswer: string;
 }): JSX.Element {
+    const [givenAnswer, setGivenAnswer] = useState<string>("");
+    function updateGivenAnswer(event: React.ChangeEvent<HTMLInputElement>) {
+        setGivenAnswer(event.target.value);
+    }
     return (
         <div>
-            <h3>Check Answer</h3>
+            <Form.Group controlId="formQuizAnswer" as={Row}>
+                <Form.Label column sm={2}>
+                    Expected Answer:
+                </Form.Label>
+                <Col>
+                    <Form.Control
+                        value={givenAnswer}
+                        onChange={updateGivenAnswer}
+                    ></Form.Control>
+                </Col>
+                <Col>{givenAnswer === expectedAnswer ? "✔️" : "❌"}</Col>
+            </Form.Group>
         </div>
     );
 }
--- src/form-components/GiveAttempts.tsx
+++ src/form-components/GiveAttempts.tsx
@@ -1,9 +1,77 @@
 import React, { useState } from "react";
+import { Button, Form } from "react-bootstrap";
+
+interface RevealRequestButtonProps {
+    setAttemptsLeft: (newAttemptsLeft: string) => void;
+    requestedAttempts: string;
+    attemptsLeft: string;
+}
+function RequestedAttemptButton({
+    setAttemptsLeft,
+    requestedAttempts,
+    attemptsLeft
+}: RevealRequestButtonProps): JSX.Element {
+    //make them integers and add them
+    const requestedAttemptstoNum: number = parseInt(requestedAttempts, 10) || 0;
+    const addedAttempts: number =
+        requestedAttemptstoNum + parseInt(attemptsLeft, 10);
+    return (
+        <Button onClick={() => setAttemptsLeft(`${addedAttempts}`)}>
+            gain
+        </Button>
+    );
+}
+interface RevealUseButtonProps {
+    setAttemptsLeft: (newAttemptsLeft: string) => void;
+    attemptsLeft: string;
+}
+function UseButton({
+    setAttemptsLeft,
+    attemptsLeft
+}: RevealUseButtonProps): JSX.Element {
+    return (
+        <Button
+            onClick={() => setAttemptsLeft(`${parseInt(attemptsLeft, 10) - 1}`)}
+            disabled={parseInt(attemptsLeft, 10) === 0}
+        >
+            use
+        </Button>
+    );
+}
 
 export function GiveAttempts(): JSX.Element {
+    const [attemptsLeft, setAttemptsLeft] = useState<string>("3");
+    const [requestedAttempts, setRequestedAttempts] = useState<string>("");
+
+    function updateAttemptsLeft(event: React.ChangeEvent<HTMLInputElement>) {
+        setAttemptsLeft(event.target.value);
+    }
+    function updateRequestedAttempts(
+        event: React.ChangeEvent<HTMLInputElement>
+    ) {
+        setRequestedAttempts(event.target.value);
+    }
+
     return (
         <div>
-            <h3>Give Attempts</h3>
+            <div>Number of attempts left: {attemptsLeft}</div>
+            <Form.Group controlId="formRequestedAttempts">
+                <Form.Label>Requested Attempts: </Form.Label>
+                <Form.Control
+                    type="number"
+                    value={requestedAttempts}
+                    onChange={updateRequestedAttempts}
+                ></Form.Control>
+            </Form.Group>
+            <RequestedAttemptButton
+                setAttemptsLeft={setAttemptsLeft}
+                requestedAttempts={requestedAttempts}
+                attemptsLeft={attemptsLeft}
+            ></RequestedAttemptButton>
+            <UseButton
+                setAttemptsLeft={setAttemptsLeft}
+                attemptsLeft={attemptsLeft}
+            ></UseButton>
         </div>
     );
 }
